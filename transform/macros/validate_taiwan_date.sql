05dafeb03a50edaccbfc95a3211dfde2eeb13e18
hung hsiang Chien <kevinchien@hungdeMacBook-Air.local>
2025-02-06 21:59:41 +0800

    add macro for convert and transform taiwan date

--- transform/macros/validate_taiwan_date.sql
+++ transform/macros/validate_taiwan_date.sql
@@ -1,7 +1,20 @@
--- macros/validate_taiwan_date.sql
-{% macro validate_taiwan_date(column) %}
-    (
-        {{ convert_taiwan_date(column) }} is not null
-        and {{ convert_taiwan_date(column) }} < current_date()
-    )
+{% macro validate_taiwan_date(column, earliest_date="2011-07-01", latest_date=None) %}
+    {# Set the latest_date expression: use CURRENT_DATE() if latest_date is not provided #}
+    {% if latest_date is none %} {% set latest_date_expr = "CURRENT_DATE()" %}
+    {% else %} {% set latest_date_expr = "DATE('" ~ latest_date ~ "')" %}
+    {% endif %}
+
+    {# If earliest_date is provided, include the lower bound check; otherwise, skip it #}
+    {% if earliest_date is none %}
+        (
+            {{ convert_taiwan_date(column) }} is not null
+            and {{ convert_taiwan_date(column) }} <= {{ latest_date_expr }}
+        )
+    {% else %}
+        (
+            {{ convert_taiwan_date(column) }} is not null
+            and {{ convert_taiwan_date(column) }} >= date('{{ earliest_date }}')
+            and {{ convert_taiwan_date(column) }} <= {{ latest_date_expr }}
+        )
+    {% endif %}
 {% endmacro %}
--- transform/models/staging/plvr/stg_plvr_land_building_park_transaction.sql
+++ transform/models/staging/plvr/stg_plvr_land_building_park_transaction.sql
@@ -8,35 +8,62 @@ with
             transaction_sign as transaction_subject,
             land_sector_position_building_sector_house_number_plate
             as land_slot_or_address,
-            land_shifting_total_area_square_meter as total_tranferred_area_square_meter,
+            safe_cast(
+                land_shifting_total_area_square_meter as float64
+            ) as total_tranferred_area_square_meter,
             the_use_zoning_or_compiles_and_checks as metropolis_zone_type,
             the_nonmetropolis_land_use_district as nonmetropolis_zone_type,
             nonmetropolis_land_use as nonmetropolis_land_use_designation,
-            transaction_year_month_and_day as transaction_date,
+            case
+                when {{ validate_taiwan_date("transaction_year_month_and_day") }}
+                then {{ convert_taiwan_date("transaction_year_month_and_day") }}
+                else null
+            end as transaction_date,
             transaction_pen_number as number_of_property_type,
             shifting_level as transferred_floor,
             total_floor_number as total_floors,
             building_state as building_type,
             main_use as primary_use,
             main_building_materials as primary_building_material,
-            construction_to_complete_the_years as construction_completion_date,
+            case
+                when
+                    {{
+                        validate_taiwan_date(
+                            "construction_to_complete_the_years", earliest_date=None
+                        )
+                    }}
+                then {{ convert_taiwan_date("construction_to_complete_the_years") }}
+                else null
+            end as construction_completion_date,
             building_shifting_total_area as building_transferred_area_square_meter,
-            building_present_situation_pattern_room as building_number_of_bedrooms,
-            building_present_situation_pattern_hall as building_number_of_living_rooms,
-            building_present_situation_pattern_health as building_number_of_bathrooms,
+            safe_cast(
+                building_present_situation_pattern_room as int64
+            ) as building_number_of_bedrooms,
+            safe_cast(
+                building_present_situation_pattern_hall as int64
+            ) as building_number_of_living_rooms,
+            safe_cast(
+                building_present_situation_pattern_health as int64
+            ) as building_number_of_bathrooms,
             building_present_situation_pattern_compartmented
             as building_number_of_partitions,
             whether_there_is_manages_the_organization as has_management_organization,
-            total_price_ntd as total_price_ntd,
-            the_unit_price_ntd_square_meter as price_per_square_meter,
+            safe_cast(total_price_ntd as float64) as total_price_ntd,
+            safe_cast(
+                the_unit_price_ntd_square_meter as int64
+            ) as price_per_square_meter,
             the_berth_category as parking_space_type,
-            berth_shifting_total_area_square_meter as parking_transferred_sqare_meter,
-            the_berth_total_price_ntd as parking_total_price_ntd,
+            safe_cast(
+                berth_shifting_total_area_square_meter as float64
+            ) as parking_transferred_sqare_meter,
+            safe_cast(the_berth_total_price_ntd as int64) as parking_total_price_ntd,
             the_note as note,
             serial_number as transaction_id,
-            main_building_area as main_building_area_square_meter,
-            auxiliary_building_area as auxiliary_building_area_square_meter,
-            balcony_area as balcony_area_square_meter,
+            safe_cast(main_building_area as float64) as main_building_area_square_meter,
+            safe_cast(
+                auxiliary_building_area as float64
+            ) as auxiliary_building_area_square_meter,
+            safe_cast(balcony_area as float64) as balcony_area_square_meter,
             elevator as has_elevator,
             transaction_number as transferred_number,
             _smart_source_bucket,
