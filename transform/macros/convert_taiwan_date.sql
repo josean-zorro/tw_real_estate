fbd3d6a3c81898b50acf6a0325008e81a579ba2a
hung hsiang Chien <kevinchien@hungdeMacBook-Air.local>
2025-02-11 12:59:32 +0800

    renew validate and convert taiwan date logic for new pattern of date type

--- transform/macros/convert_taiwan_date.sql
+++ transform/macros/convert_taiwan_date.sql
@@ -2,8 +2,9 @@
     safe.parse_date(
         '%Y%m%d',
         safe_cast(
-            19110000
-            + safe_cast(regexp_replace({{ column }}, '^0+', '') as int64) as string
+            19110000 + safe_cast(
+                regexp_replace({{ column }}, r'(^0+)|(\.0)|(\s+)', '') as int64
+            ) as string
         )
     )
 {% endmacro %}
