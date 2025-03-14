38d6b1bc02a246fd3fcbfb1d8b900fe627031063
hung hsiang Chien <kevinchien@hungdeMacBook-Air.local>
2025-03-14 13:16:35 +0800

    optimize date dataflows and add year_month_number to dim_date

--- transform/models/marts/dim_date.sql
+++ transform/models/marts/dim_date.sql
@@ -1,17 +1,10 @@
 {{ config(materialized="table") }}
 
-{{ config(materialized="table") }}
+with
+    result as (
+        select *, concat(year_number, '-', month_of_year) as year_month_number
+        from {{ ref("date") }}
+    )
 
-{% set current_date_sql %}
-    SELECT date_add(CURRENT_DATE("Asia/Taipei"), interval 1 day) AS today_date
-{% endset %}
-
-{% if execute %}
-    {% set results = run_query(current_date_sql) %}
-    {% set today_date = results.columns[0].values()[0] %}
-{% else %}
-    {# Fallback value during compile time #}
-    {% set today_date = "2020-01-01" %}
-{% endif %}
-
-{{ dbt_date.get_date_dimension("1911-01-01", today_date) }}
+select *
+from result
