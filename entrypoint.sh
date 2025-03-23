a8bf18a431587558b240be2e21ad5be34b02ce8e
hung hsiang Chien <kevinchien@hungdeMacBook-Air.local>
2025-03-23 15:13:59 +0800

    fix: copy dbt docs from Docker container to GitHub Actions workspacefix docs generate of dbt docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,11 @@
 cd /project
 echo "Running '$*'"
 eval $*
+
+case "$*" in
+  *"dbt-bigquery:docs-generate"*)
+    echo "Copying dbt docs to /github/workspace/dbt-docs..."
+    mkdir -p /github/workspace/dbt-docs
+    cp -r .meltano/transformers/dbt/target/* /github/workspace/dbt-docs/ || echo "Nothing to copy"
+    ;;
+esac
